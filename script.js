a0c635007d922cf678df483b65c6cf0aed732211
tonyshark17b-cell <tonyshark17b@gmail.com>
2026-07-12 23:12:13 -0500

    responsive menu bar

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
+const butten = document.querySelector(".slide")
+const bar = document.querySelector(".bar")
+butten.addEventListener("click", () => {
+    bar.classList.toggle("active");
+    console.log("hello");
 
+})
 
