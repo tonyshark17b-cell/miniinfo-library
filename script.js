ea1553c9ede38147ec9abb620e49ffd6dd8a66e3
tonyshark17b-cell <tonyshark17b@gmail.com>
2026-07-14 14:57:58 -0500

    wworking search bar

--- script.js
+++ script.js
@@ -1,35 +1,55 @@
 const butten = document.querySelector(".slide")
 const bar = document.querySelector(".bar")
-butten.addEventListener("click", () => {
-    bar.classList.toggle("active");
-    console.log("hello");
-
-})
 const input = document.querySelector(".search")
 const suggesion = document.querySelector(".suggesion")
 const cards = document.querySelectorAll(".box")
 const search = document.querySelector(".in")
+const body = document.querySelector("body")
 
+butten.addEventListener("click", (e) => {
+    e.stopPropagation();
+    bar.classList.toggle("active");
+    input.value = "";
+    suggesion.innerHTML = "";
+})
+bar.addEventListener("click", (e) => {
+    e.stopPropagation();
+})
+body.addEventListener("click", () => {
+    bar.classList.remove("active");
+})
 input.addEventListener("input", () => {
     const text = input.value.toLowerCase()
     suggesion.innerHTML = "";
     cards.forEach(card => {
         const h2 = card.querySelector(".title");
-        console.log(h2)
         const title = h2.textContent.toLowerCase();
         if (title.startsWith(text) && text !== "") {
             card.style.display = "block"
             const div = document.createElement("div")
             div.textContent = h2.textContent;
-            suggesion.appendChild(div)
+            suggesion.appendChild(div);
+            div.addEventListener("click", () => {
+                input.value = div.textContent;
+                cards.forEach(card => {
+                    const title = card.querySelector(".title").textContent;
+                    if (title === div.textContent) {
+                        card.style.display = "block";
+                    } else {
+                        card.style.display = "none";
+                    }
+                    suggesion.innerHTML = "";
+
+                })
+            })
         } else {
             card.style.display = "none"
         }
         if (text === "") {
             cards.forEach(card => {
                 card.style.display = "block";
+                suggesion.innerHTML = "";
             })
-            suggesion.innerHTML = "";
         }
 
     });
