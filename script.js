de8607dcab954d5acb0468d3faad6ed9c24fa35d
tonyshark17b-cell <tonyshark17b@gmail.com>
2026-07-14 00:40:30 -0500

    newcard collection

--- script.js
+++ script.js
@@ -5,4 +5,32 @@ butten.addEventListener("click", () => {
     console.log("hello");
 
 })
+const input = document.querySelector(".search")
+const suggesion = document.querySelector(".suggesion")
+const cards = document.querySelectorAll(".box")
+const search = document.querySelector(".in")
 
+input.addEventListener("input", () => {
+    const text = input.value.toLowerCase()
+    suggesion.innerHTML = "";
+    cards.forEach(card => {
+        const h2 = card.querySelector(".title");
+        console.log(h2)
+        const title = h2.textContent.toLowerCase();
+        if (title.startsWith(text) && text !== "") {
+            card.style.display = "block"
+            const div = document.createElement("div")
+            div.textContent = h2.textContent;
+            suggesion.appendChild(div)
+        } else {
+            card.style.display = "none"
+        }
+        if (text === "") {
+            cards.forEach(card => {
+                card.style.display = "block";
+            })
+            suggesion.innerHTML = "";
+        }
+
+    });
+})
